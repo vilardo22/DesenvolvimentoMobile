214e79b9c65c25dabe371870b985415ea44a41b9
vilardo22 <gabrielvilardo22@gmail.com>
2026-08-20 21:26:21 -0300

    PRIMEIROS CODIGOS

--- app/src/main/java/com/example/myapplicationaula4/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationaula4/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.myapplicationaula4.ui.theme.MyApplicationAula4Theme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +24,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationAula4Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Gabriel",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,9 +34,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun ExemploAula1(
+    texto: String,
+    tamanho: Int
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = texto,
+            fontSize = tamanho.sp
+        )
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello, $name!",
         modifier = modifier
     )
 }
